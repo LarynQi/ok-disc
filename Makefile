963e383d19fc1c42efd7ebfc99825e424fa4414d
Laryn Qi <larynqi@berkeley.edu>
2020-11-07 17:46:03 -0800

    v0.1.5

--- Makefile
+++ Makefile
@@ -1,11 +1,94 @@
+all:
+# 	make build
+	@echo Not implemented
+
 run:
 	python3 ok-disc.py -v
+
+DS_STORE = ./build/.DS_Store
+ifneq ("$(wildcard $(DS_Store))","")
+	DEL_DS = rm $(DS_Store)
+else 
+	DEL_DS = @:
+endif
+
+# ifeq (build,$(firstword $(MAKECMDGOALS)))
+# 	BUILD_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+# 	$(eval $(BUILD_ARGS):dummy;@:)
+# endif
+
+.PHONY: build
+
 build:
+	@$(delete)
+	@python3 -m zipapp build -m "ok_disc:main" -o ok
+	@make clean
+	@mkdir out
+	@mv ok ./out/ok
+	@cp ./assets/code/scheme ./out/scheme
+#   https://erictleung.com/user-input-makefile
+	@cp ./src/$(filter-out $@,$(MAKECMDGOALS)) ./out/
+
+	@echo Files can be found in ./out
+	@echo 'Use make zip <name-of-zip> to generate zip'
+
+# for command line args
+%: 
+	@:
+
+
+ZIP = $(filter-out $@,$(MAKECMDGOALS))
+ifneq ("$(wildcard $(ZIP))","")
+	DEL_ZIP = rm -r $(ZIP)
+else 
+	DEL_ZIP = @:
+endif
+
+ZIPS = ./zips
+ifeq ("$(wildcard $(ZIPS))","")
+	MK_ZIPS = mkdir $(ZIPS)
+	DEL_ZIPS = @:
+else 
+	MK_ZIPS = @:
+	DEL_ZIPS = rm -r $(ZIPS)
+endif
+
+
+zip:
+	$(MK_ZIPS)
+	$(DEL_ZIP)
+	mkdir $(filter-out $@,$(MAKECMDGOALS))
+	cp -a ./out/. $(filter-out $@,$(MAKECMDGOALS))/
+	zip -r ./zips/$(filter-out $@,$(MAKECMDGOALS)).zip $(filter-out $@,$(MAKECMDGOALS))
+	rm -r $(filter-out $@,$(MAKECMDGOALS))
+
+zip-inner:
+	python3 -m zipapp ok_disc -m "ok_disc:main"
+
+build-deprecated:
 	python3 -m compileall ok-disc.py
 	mv ./__pycache__/ok-disc.cpython-37.pyc ok
 	rm -r ./__pycache__
 
+build-windows:
+	python3 -m compileall ok-disc-win.py
+	mv ./__pycache__/ok-disc-win.cpython-37.pyc ok
+	rm -r ./__pycache__
+
+build-disc:
+	python3 -m compileall ok-disc-build.py
+	mv ./__pycache__/ok-disc-build.cpython-37.pyc ok
+	rm -r ./__pycache__
+
+OUT = ./out
+ifneq ("$(wildcard $(OUT))","")
+	CLEAN = @rm -r $(OUT)
+else 
+	CLEAN = @:
+endif
+
 clean:
-	rm ok
-	rm scheme
-	
+	$(CLEAN)
+
+clean-zips:
+	$(DEL_ZIPS)
--- ok-disc.py
+++ ok-disc.py
@@ -1,167 +1,91 @@
-#!/usr/bin/env python3
+import sys
+
+VERSION_MESSAGE = "\n" + """
+ERROR: You are using Python {}.{}, but ok-disc is developed on Python 3.7.
+This can lead to unexpected behavior. Make sure you are using the right
+command (e.g. `python3 ok` instead of `python ok`) and that you have
+Python 3.7+ installed.
+""".strip() + "\n"
+
+if sys.version_info[:2] < (3, 7):
+    print(VERSION_MESSAGE.format(*sys.version_info[:2]))
+    sys.exit(1)
+
 import subprocess
 import re
 import argparse
-import sys
 import urllib.request
 import ssl
 import platform
 import os
-from datetime import datetime
-from datetime import timezone
-from dateutil import tz
 
 parser = argparse.ArgumentParser(description="Test your work")
 parser.add_argument("func", metavar="function_to_test", nargs="?", help="Function to be tested")
 parser.add_argument("-v", dest="v", action="store_const", const=True, default=False, help="Verbose output")
-parser.add_argument("--decrypt", dest="decrypt", action="store_const", const=True, default=False, help="Decrypt next exam question")
 args = parser.parse_args()
 
-EXTENSIONS = ("py", "scm", "sql")
-ssl._create_default_https_context = ssl._create_unverified_context
 src = ""
-q, extension = src.split(".")
-PDT = tz.gettz("US/Pacific")
-# TIME = datetime(2020, 8, 8, 1, 30, tzinfo=PDT)
-now = datetime.now(tz=timezone.utc)
-if args.decrypt:
-    diff = (TIME - now).total_seconds()
-    if diff <= 0:
-        if src in os.listdir():
-            sys.exit("Already decrypted")
-        url = f"https://larynqi.github.io/assets/code/{src}"
-        # url = f"https://github.com/LarynQi/LarynQi.github.io/raw/master/assets/code/{src}"
-        try:
-            urllib.request.urlretrieve(url, src)
-        except Exception as e:
-            print(url)
-            print(e)
-        if extension == "scm" and "scheme" not in os.listdir():
-            url = "https://github.com/LarynQi/LarynQi.github.io/raw/master/assets/scheme"
-            urllib.request.urlretrieve(url, "scheme")
-        elif extension == "sql" and "sqlite3" not in os.listdir():
-            url = "https://github.com/LarynQi/LarynQi.github.io/raw/master/assets/sqlite3"
-            urllib.request.urlretrieve(url, "sqlite3")
-    else:
-        local = tz.gettz()
-        local = TIME.astimezone(local)
-        days = int(diff // (3600 * 24))
-        hours = int((diff // 3600) % 24)
-        minutes = int((diff // 60) % 60)
-        seconds = int(diff % 60)
-        days_s = f"{days} days " if days > 0 else ""
-        hours_s = f"{hours} hours " if hours > 0 else ""
-        minutes_s = f"{minutes} minutes " if minutes > 0 else ""
-        seconds_s = f"{seconds} seconds"
-        sys.exit(f"Unlock the next question at {local.strftime('%m/%d/%Y %H:%M:%S %Z')} (in {days_s}{hours_s}{minutes_s}{seconds_s})")
-    sys.exit(0)
-
 if src not in os.listdir():
-    sys.exit("You must decrypt first. If you've decrypted already, please do not change the names of the provided files")
-version = "0.1.4 - Exam Tool"
-
-
-
-if extension == "scm" and "scheme" not in os.listdir():
+    sys.exit("Please do not change the names of the provided files")
+version = "0.1.5"
+if "scheme" not in os.listdir():
     url = "https://github.com/LarynQi/LarynQi.github.io/raw/master/assets/scheme"
+    ssl._create_default_https_context = ssl._create_unverified_context
     urllib.request.urlretrieve(url, "scheme")
 
-if extension == "sql" and "sqlite3" not in os.listdir():
-    url = "https://github.com/LarynQi/LarynQi.github.io/raw/master/assets/sqlite3"
-    urllib.request.urlretrieve(url, "sqlite3")
-
 system = platform.system()
-if system == "Darwin":
+if system != "Windows":
     python = "python3"
 else:
     python = "python"
 
 class Doctest():
 
-    def __init__(self, language, test, output=""):
-        self.language = language
+    def __init__(self, test, output=""):
         self.test = test
         self.output = output
 
     def run(self, actual):
-        base = f"{self._convert()} {self.test}\n{self.output}\n"
-        if actual.strip() == self.output.strip():
-            return f"{base}-- OK! --\n", True
+        base = "scm> {!s}\n{!s}\n".format(self.test, self.output)
+        if actual == self.output:
+            return base + "-- OK! --\n", True
         tab = "     "
         spaced_actual = ""
         for c in actual:
             if c == "\n":
-                spaced_actual += f"{c}{tab}"
+                spaced_actual = c + tab
             else:
                 spaced_actual += c
-        spaced_output = ""
-        for c in self.output:
-            if c == "\n":
-                spaced_output += f"{c}{tab}"
-            else:
-                spaced_output += c
-        return f"{base}Error: expected\n     {spaced_output}\nbut got\n{tab}{spaced_actual}\n", False
+        return base + "Error: expected\n     {!s}\nbut got\n{!s}{!s}\n".format(self.output, tab, spaced_actual), False
 
-    def _convert(self):
-        if self.language == "scheme":
-            return "scm>"
-        elif self.language == "sql":
-            return "sqlite3>"
     def __str__(self):
-        return f"Input: {self.test}, Output: {self.output}"
+        return "Input: {!s}, Output: {!s}".format(self.test, self.output)
 
-if extension == "scm":
-    doctest = re.compile(r"; Q.* - .*")
-    expect = re.compile(r"; expect .*")
-    test = re.compile(r"; .*\n")
-    no_tests = re.compile(r";;; No Tests\n")
-    buf = re.compile(r";;; Tests\n")
-elif extension == "sql":
-    doctest = re.compile(r"-- Q.* - .*")
-    expect = re.compile(r"-- expect .*")
-    test = re.compile(r"-- .*\n")
-    no_tests = re.compile(r"-- No Tests\n")
-    buf = re.compile(r"-- Tests\n")
+doctest = re.compile(r"; Q\d(\.\d)* - .*")
+expect = re.compile(r"; expect .*")
+test = re.compile(r"; .*\n")
+no_tests = re.compile(r";;; No Tests\n")
+buf = re.compile(r";;; Tests\n")
 
 tests = {}
-if extension == "sql":
-    with open(src, "r") as f:
-        found = False
-        for line in f:
-            if found:
-                if re.match(buf, line):
-                    pass
-                elif re.match(expect, line):
-                    tests[question][len(tests[question]) - 1].output += line.strip()[10:] + "\n"
-                elif line == "\n" or re.match(no_tests, line):
-                    found = False
-                elif re.match(test, line):
-                    tests[question] = tests.get(question, []) + [Doctest(extension, line.strip()[3:])]
-                else:
-                    print(line)
-                    sys.exit("Invalid doctest format")
-            elif re.match(doctest, line):
-                found = True
-                question = line[3:line.index("\n")]
-elif extension == "scm":
-    with open(src, "r") as f:
-        found = False
-        for line in f:
-            if found:
-                if re.match(buf, line):
-                    pass
-                elif re.match(expect, line):
-                    tests[question][len(tests[question]) - 1].output += line.strip()[9:] + "\n"
-                elif line == "\n" or re.match(no_tests, line):
-                    found = False
-                elif re.match(test, line):
-                    tests[question] = tests.get(question, []) + [Doctest("scheme", line.strip()[2:])]
-                else:
-                    print(line)
-                    sys.exit("Invalid doctest format")
-            elif re.match(doctest, line):
-                found = True
-                question = line[2:line.index("\n")]
+with open(src, "r") as f:
+    found = False
+    for line in f:
+        if found:
+            if re.match(buf, line):
+                pass
+            elif re.match(expect, line):
+                tests[question][len(tests[question]) - 1].output += line.strip()[9:] + "\n"
+            elif line == "\n" or re.match(no_tests, line):
+                found = False
+            elif re.match(test, line):
+                tests[question] = tests.get(question, []) + [Doctest(line.strip()[2:])]
+            else:
+                print(line)
+                sys.exit("Invalid doctest format")
+        elif re.match(doctest, line):
+            found = True
+            question = line[2:line.index("\n")]
 
 if args.func:
     remove = list(filter(lambda func: func[-len(args.func):] == args.func, tests.keys()))
@@ -171,162 +95,59 @@ if args.func:
         sys.exit("Invalid function name")
     tests = {key:tests[key] for key in tests.keys() if key == remove[0]}
 
-print(f"=====================================================================\nAssignment: Discussion 13 {'q' + q[-1]}\nOK-disc, version v{version}\
+print("=====================================================================\nAssignment: Discussion {!s}\nOK-disc, version v{!s}\
 \n=====================================================================\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\
-\nRunning tests\n")
+\nRunning tests\n".format(src[src.index('.') - 2:src.index('.')], version))
 
 prev = None
 total = correct = 0
-result = save = ""
-
-if extension == "py":
-    v = "-v" if args.v else ""
-    interpreter = subprocess.Popen([python, "-B", "-m", "doctest", src, "-v"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=0)    
-    with open(src, "r") as f:
-        for line in f:
-            if line[:3] == "def":
-                q = line[4:line.index("(")]
-    if args.v:
-        result += f"---------------------------------------------------------------------\nDoctests for {q}\n\n"
-        expected_out = re.compile(r"Expected:\n.*Got:\n")
-        count = 1
-        found_expected = found_got = ok = False
-        for line in interpreter.stdout:
-            if line == "Trying:\n":
-                result += f"Case {count}:\n>>> "
-                found = True
-                count += 1
-                total += 1
-            elif line == "Expecting:\n":
-                found_expected = True
-            elif line == "Got:\n":
-                save = f"\nError: expected\n    {save}but got\n    "
-                found_got = True
-            elif line == "ok\n":
-                result += save + "-- OK! --\n\n"
-                correct += 1
-            elif found_expected:
-                save = line.strip() + "\n"
-                found_expected = False
-            elif found_got:
-                result += f"{line.strip()}\n{save}{line.strip()}\n\n"
-                found_got = False
-            elif found:
-                result += line.strip() + "\n"
-                found = False
-    else:
-        expected_out = re.compile(r"Expected:\n.*Got:\n")
-        count = 1
-        found_expected = found_got = ok = False
-        for line in interpreter.stdout:
-            if line == "Trying:\n":
-                save = f"Case {count}:\n>>> "
-                found = True
-                count += 1
-                total += 1
-            # elif line == "Expected:\n":
-            #     result += f"Error: expected\n"
-            #     found = True
-            elif line == "Expecting:\n":
-                found_expected = True
-            elif line == "Got:\n":
-                save += f"\nError: expected\n    {expected}but got\n    "
-                found_got = True
-            elif line == "ok\n":
-                correct += 1
-            elif found_expected:
-                expected = line.strip() + "\n"
-                found_expected = False
-            elif found_got:
-                line = line.strip() + "\n"
-                result += f"{save[:save.index('Error') - 1] + line + save[save.index('Error') - 1:]}{line.strip()}\n\n"
-                break
-            elif found:
-                save += line.strip() + "\n"
-                found = False
-if extension == "scm":
-    check = '(load-all ".")'
+check = '(load-all ".")'
+scheme = subprocess.Popen([python, "scheme", "-i", src], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=0)
+parens = scheme.communicate(input=check)
+for line in parens:
+    if "SyntaxError" in line:
+        sys.exit("scm> {!s}\n # Error: unexpected end of file\n".format(check))
+scheme.stdin.close()
+result = ""
+while tests:
     scheme = subprocess.Popen([python, "scheme", "-i", src], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=0)
-    parens = scheme.communicate(input=check)
-    for line in parens:
-        if "SyntaxError" in line:
-            sys.exit(f"scm> {check}\n # Error: unexpected end of file\n")
-    scheme.stdin.close()
-    while tests:
-        scheme = subprocess.Popen([python, "scheme", "-i", src], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=0)
-        for line in scheme.stdout:
+    for line in scheme.stdout:
+        break
+    if tests:
+        t = next(iter(tests.keys()))
+        if not prev:
+            count = 1
+            if args.v:
+                result += "---------------------------------------------------------------------\nDoctests for {!s}\n\n".format(t[t.index('-') + 2:])
+            prev = t
+        curr = tests[t][0]
+        curr.output = curr.output.strip()
+        raw_out = scheme.communicate(input=curr.test)[0]   
+        test_out = curr.run(raw_out[:raw_out.index("\nscm>")])
+        total += 1
+        if args.v:
+            result += "Case {!s}:\n".format(count)
+            result += "{!s}\n".format(test_out[0])
+        elif not test_out[1]:
+            result += "---------------------------------------------------------------------\nDoctests for {!s}\n\n".format(t[t.index('-') + 2:])
+            result += "Case {!s}:\n".format(count)
+            result += "{!s}\n".format(test_out[0])
             break
-        if tests:
-            t = next(iter(tests.keys()))
-            if not prev:
-                count = 1
-                if args.v:
-                    result += f"---------------------------------------------------------------------\nDoctests for {t[t.index('-') + 2:]}\n\n"
-                prev = t
-            curr = tests[t][0]
-            curr.output = curr.output.strip()
-            raw_out = scheme.communicate(input=curr.test)[0]   
-            test_out = curr.run(raw_out[:raw_out.index("\nscm>")])
-            total += 1
-            if args.v:
-                result += f"Case {count}:\n"
-                result += f"{test_out[0]}\n"
-            elif not test_out[1]:
-                result += f"---------------------------------------------------------------------\nDoctests for {t[t.index('-') + 2:]}\n\n"
-                result += f"Case {count}:\n"
-                result += f"{test_out[0]}\n"
-                break
-            correct += int(test_out[1])
-            tests[t].remove(curr)
-            count += 1
-            if not tests[t]:
-                tests.pop(t)
-                prev = None
-        scheme.stdin.close()
-if extension == "sql":
-    timeout = 0
-    while tests:
-        timeout += 1
-        if timeout >= 1000:
-            sys.exit("Timed out")
-        interpreter = subprocess.Popen(["sqlite3", "--init", src], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, bufsize=0)
-        if tests:
-            t = next(iter(tests.keys()))
-            if not prev:
-                count = 1
-                if args.v:
-                    result += f"---------------------------------------------------------------------\nDoctests for {t[t.index('-') + 2:]}\n\n"
-                prev = t
-            curr = tests[t][0]
-            curr.output = curr.output.strip()
-            raw_out = interpreter.communicate(input=curr.test) 
-            if raw_out[0] == "":
-                raw_out = raw_out[1]
-            test_out = curr.run(raw_out[:])
-            total += 1
-            if args.v:
-                result += f"Case {count}:\n"
-                result += f"{test_out[0]}\n"
-            elif not test_out[1]:
-                result += f"---------------------------------------------------------------------\nDoctests for {t[t.index('-') + 2:]}\n\n"
-                result += f"Case {count}:\n"
-                result += f"{test_out[0]}\n"
-                break
-            correct += int(test_out[1])
-            tests[t].remove(curr)
-            count += 1
-            if not tests[t]:
-                tests.pop(t)
-                prev = None
-        interpreter.stdin.close()
+        correct += int(test_out[1])
+        tests[t].remove(curr)
+        count += 1
+        if not tests[t]:
+            tests.pop(t)
+            prev = None
+    scheme.stdin.close()
 
-result += f"---------------------------------------------------------------------\nTest summary\n"
+result += "---------------------------------------------------------------------\nTest summary\n"
 if args.v:
-    result += f"    Passed: {correct}\n    Failed: {total - correct}\n[ooooook....] {100 * round(correct / total, 3)}% passed\n"
+    result += "    Passed: {!s}\n    Failed: {!s}\n[ooooook....] {!s}% passed\n".format(correct, total - correct, 100 * round(correct / total, 3))
 else:
     if correct == total:
-        result += f"    {correct} test cases passed! No cases failed.\n"
+        result += "    {!s} test cases passed! No cases failed.\n".format(correct)
     else:
-        result += f"    {correct} test case(s) passed before encountering first failed test case\n"
+        result += "    {!s} test case(s) passed before encountering first failed test case\n".format(correct)
 
 print(result)
